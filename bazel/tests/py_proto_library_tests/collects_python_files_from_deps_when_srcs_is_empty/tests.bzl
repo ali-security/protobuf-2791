protocolbuffers/protobuf

Migrate Starlark tests to new format with one test per directory.
PiperOrigin-RevId: 874691470

bazel/tests/py_proto_library_tests/collects_python_files_from_deps_when_srcs_is_empty/tests.bzl
@@ -1,0 +1,11 @@
+"""Test suite for py_proto_library that collects Python files from deps when srcs is empty."""
+
+def test_collects_python_files_from_deps_when_srcs_is_empty(env, target):
+    runfiles_paths = [f.basename for f in target[DefaultInfo].default_runfiles.files.to_list()]
+
+    expected_basename = "b_proto_pb2.py"
+    env.expect.that_collection(runfiles_paths).contains(expected_basename)
+
+TESTS = [
+    (test_collects_python_files_from_deps_when_srcs_is_empty, ":a_py_pb2"),
+]

ali-security/protobuf-2791

bazel/tests/py_proto_library_tests/collects_python_files_from_deps_when_srcs_is_empty/tests.bzl
@@ -1,0 +1,11 @@
+"""Test suite for py_proto_library that collects Python files from deps when srcs is empty."""
+
+def test_collects_python_files_from_deps_when_srcs_is_empty(env, target):
+    runfiles_paths = [f.basename for f in target[DefaultInfo].default_runfiles.files.to_list()]
+
+    expected_basename = "b_pb2.py"
+    env.expect.that_collection(runfiles_paths).contains(expected_basename)
+
+TESTS = [
+    (test_collects_python_files_from_deps_when_srcs_is_empty, ":a_py_pb2"),
+]